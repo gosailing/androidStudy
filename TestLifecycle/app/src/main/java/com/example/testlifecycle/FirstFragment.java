029c66804a360088fb3622adc72651fcab7f48fd
YH <gosailing@126.com>
2021-06-26 09:39:36 +0800

    use androidx fragment

--- TestLifecycle/app/src/main/java/com/example/testlifecycle/FirstFragment.java
+++ TestLifecycle/app/src/main/java/com/example/testlifecycle/FirstFragment.java
@@ -1,12 +1,12 @@
 package com.example.testlifecycle;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import com.example.testlifecycle.databinding.FirstFragmentMainBinding;
 
 public class FirstFragment extends Fragment {
--- TestLifecycle/app/src/main/java/com/example/testlifecycle/MainActivity.java
+++ TestLifecycle/app/src/main/java/com/example/testlifecycle/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.testlifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirstFragment firstFragment =
-                getSupportFragmentManager().findFragmentByTag(FirstFragment.class.getName());
+                (FirstFragment) getSupportFragmentManager().findFragmentByTag(FirstFragment.class.getName());
         if (firstFragment == null) {
             firstFragment = FirstFragment.newInstance();
             ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), R.id.fragment_content, firstFragment, FirstFragment.class.getName());
